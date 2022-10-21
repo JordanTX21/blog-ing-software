8fe30117ac1cd397082c2050da5989ed116d0714
JordanTX21 <jordantx21@gmail.com>
2022-10-21 00:42:04 -0500

    build

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="about">
-    <p>Noblejas Saavedra Jordan Moises</p>
-    <p>Hernandez Mio Dessireth</p>
-    <p>Rodriguez Huaman Sebastian</p>
-    <p>Melo Jesus</p>
-    <p>Infantas Ccoicca Giancarlos</p>
-    <p>Vera Yauri Victor</p>
+  <div>
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+      <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+            <div class="site-heading">
+              <h1>Blog Grupo 4</h1>
+              <span class="subheading">Blog del curso de ingenieria de software</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <div class="about">
+            <p>Noblejas Saavedra Jordan Moises</p>
+            <p>Hernandez Mio Dessireth</p>
+            <p>Rodriguez Huaman Sebastian</p>
+            <p>Melo Jesus</p>
+            <p>Infantas Ccoicca Giancarlos</p>
+            <p>Vera Yauri Victor</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
